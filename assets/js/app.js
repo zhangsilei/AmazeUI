12dafc455dd2cc14859694f2f8bb852675074c84
DreamOn324 <1055191226@qq.com>
2015-09-01 18:25:51 +0800

    初始化

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+/*需要提前加载的组件*/
 (function($) {
   'use strict';
 
@@ -10,5 +11,28 @@
     $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
       $fullText.text($.AMUI.fullscreen.isFullscreen ? '退出全屏' : '开启全屏');
     });
-  });
-})(jQuery);
+  });  
+
+  /*首页轮播图按钮*/
+  $('.btn-loading-example').click(function () {
+    var $btn = $(this)
+    $btn.button('loading');
+      setTimeout(function(){
+        $btn.button('reset');
+    }, 5000);
+  });  
+})(jQuery);     
+
+$(document).ready(function(){
+  var showCount = -1;   // 每次点击更多显示一个电影信息
+  var lis = $("#movie-list").children("li");   
+  lis.css("display","none");
+  $("#more").click(function(){ 
+    if(showCount > lis.length){
+      return;
+    }else{
+      showCount++;   
+      $("#movie-list").children("li").eq(showCount).css("display","block");
+    }
+  })
+})
