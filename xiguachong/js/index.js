e6ef438d19a3d35fbaf0e48ad7bd6be46b35a989
DreamOn324 <1055191226@qq.com>
2015-09-14 19:26:46 +0800

    init

--- xiguachong/js/index.js
+++ xiguachong/js/index.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
-  
-  /*if(navigator.indexOf("UCBroswer") != -1){
-    
-  }else{
-    
-  }*/
+  // 顶部tab切换
+  $(".top > a").click(function(){
+    $(".top-a-current").removeClass("top-a-current");
+    $(this).addClass("top-a-current");    
+  })
+  // 播放按钮
+  var bTop = ($(".video").height()-$(".play").height())/2;
+  var bLeft = ($(".video").width()-$(".play").width())/2;
+  $(".play").css("top",bTop);
+  $(".play").css("left",bLeft);    
+  // 众筹简介 
+  $(".item div").css("top",$(".video").css("height"));
+  $(".item div").css("height",$(".video").css("height"));
+  // 简介内容
+  var tWidth = $(".title").width();
+  $(".title").css("left",($(".item div").width()-tWidth)/2);
 })
-
-/*var UCBroswer = function(){
-  $(".content-left-logo").css("marginLeft","2rem");
-}  */  
 
 
 
