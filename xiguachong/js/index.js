15419edf7c9e6714d820f0488cf094f410df1340
DreamOn324 <1055191226@qq.com>
2015-09-14 13:47:48 +0800

    init

--- xiguachong/js/index.js
+++ xiguachong/js/index.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
-	/* 侧边栏关闭监听 */
-  
-})
+	/* 浏览器样式兼容 */
+  fitBrowser();
+})    
+
+
+/**
+ * 浏览器兼容
+ */
+var fitBrowser = function(){
+   if(navigator.userAgent.indexOf("Firefox") != -1){     // ff
+    // logo
+    $(".content-left-logo").addClass("ff-content-left-logo");   
+    $(".content-left-logo").removeClass("content-left-logo");
+    // 微信   
+    $(".content-left-weixin-img").addClass("ff-content-left-weixin-img"); 
+    $(".content-left-weixin-img").removeClass("content-left-weixin-img"); 
+    $(".content-left-weixin-span").addClass("ff-content-left-weixin-span");
+    $(".content-left-weixin-span").removeClass(".content-left-weixin-span");
+    // 微博  
+    $(".content-left-weibo-img").addClass("ff-content-left-weibo-img"); 
+    $(".content-left-weibo-img").removeClass("content-left-weibo-img"); 
+    $(".content-left-weibo-span").addClass("ff-content-left-weibo-span");
+    $(".content-left-weibo-span").removeClass(".content-left-weibo-span");
+  }
+}
+
 	
+ 
