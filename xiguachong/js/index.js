b2dec454d1994a2af08060658f9b2990147cedb6
DreamOn324 <1055191226@qq.com>
2015-09-14 14:49:20 +0800

    init

--- xiguachong/js/index.js
+++ xiguachong/js/index.js
@@ -1,34 +1,16 @@
 $(document).ready(function(){
-	/* 浏览器样式兼容 */
-  //fitBrowser();
   if(navigator.indexOf("UCBroswer") != -1){
-    alert("uc浏览器")
+    
   }else{
-    alert("非uc浏览器")
+    
   }
-})    
+})
+
+var UCBroswer = function(){
+  $(".content-left-logo").css("marginLeft","2rem");
+}    
 
 
-/**
- * 浏览器兼容
- */
-var fitBrowser = function(){
-   if(navigator.userAgent.indexOf("webKit") != -1){     // ff
-    // logo
-    $(".content-left-logo").addClass("ff-content-left-logo");      
-    $(".content-left-logo").removeClass("content-left-logo");
-    // 微信   
-    $(".content-left-weixin-img").addClass("ff-content-left-weixin-img"); 
-    $(".content-left-weixin-img").removeClass("content-left-weixin-img"); 
-    $(".content-left-weixin-span").addClass("ff-content-left-weixin-span");
-    $(".content-left-weixin-span").removeClass(".content-left-weixin-span");
-    // 微博  
-    $(".content-left-weibo-img").addClass("ff-content-left-weibo-img"); 
-    $(".content-left-weibo-img").removeClass("content-left-weibo-img"); 
-    $(".content-left-weibo-span").addClass("ff-content-left-weibo-span");
-    $(".content-left-weibo-span").removeClass(".content-left-weibo-span");
-  }
-}
 
 	
  
